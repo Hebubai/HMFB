1194c904b5c38737c9376dd38f58ce14f07933e5
zhang <zhang@MacBook-Pro.local>
2020-04-24 22:27:54 +0800

    fix

--- Files/ffmpeg.java
+++ Files/ffmpeg.java
@@ -3,29 +3,18 @@ package tcp;
 import java.io.*;
 
 public class ffmpeg {
-
-	/*
-	 * foldPath：源视频文件夹的地址
-	 * ffmpegCommand：ffmpeg的地址与命令参数。
-	 * 		可追加多线程参数，例如4线程：-threads 4
-	 * codeCommand：转码or编码的命令参数。
-	 * 		仅封装不重新编码（速度快）使用" -vcodec copy -acodec copy "	注意双引号内部左右有空格
-	 * 		重新编码（速度极慢）使用" "	注意双引号内有空格
-	 */
-	final static String foldPath = "/Users/zhang/Movies/";
+	final static String foldPath = "/Users/zhang/Downloads/";
 	final static String ffmpegCommand = "/Users/zhang/ffmpeg/bin/ffmpeg -i ";
 	final static String codeCommand = " -vcodec copy -acodec copy ";
+	final static String outputPath = "";
+	final static String extName = ".mp4";
+	static int number = 0;
 
-	/*
-	 * 提示：若转码需求变更，请修改上方静态代码。
-	 * 警告：下方所有代码不可修改。
-	 */
 	public static void main(String[] args) throws IOException {
-		//codeVideos(foldPath);
-		File[] fold = new File(foldPath).listFiles();
-		for(File f:fold) {
-			System.out.println(getName0(f.toString()));
-		}
+		long startTime = System.currentTimeMillis();
+		codeVideos(foldPath);
+		double usedTime = (double) (System.currentTimeMillis() - startTime) / 1000;
+		System.out.println("耗时：" + usedTime + " 秒。文件与文件夹一共" + number + "个。");
 	}
 
 	public static void codeVideos(String path) throws IOException {
@@ -34,34 +23,27 @@ public class ffmpeg {
 			if (f.isDirectory()) {
 				codeVideos(f.toString());
 			} else if (!f.toString().equals(foldPath + ".DS_Store") && !f.toString().equals(foldPath + ".localized")) {
-				final String command = ffmpegCommand + f.toString() + codeCommand + getName0(f.toString()) + ".mp4";
+				final String command = ffmpegCommand + f.toString() + codeCommand + outputPath + getName(f.toString());
+				new File(outputPath).mkdirs();
 				Runtime.getRuntime().exec(command);
 				System.out.println(f.toString());
-				System.out.println("Done");
 			}
 		}
 	}
 
-	/*
-	 * 将文件在系统中的全限定名中"."后所有的内容删去。
-	 * 例如：寄明月.flv  >>>  寄明月
-	 * 例如：记录2020.03.12.mp4  >>>  记录2020
-	 */
 	public static String getName(String filename) {
-		if ((filename != null) && (filename.length() > 0)) {
-			int dot = filename.indexOf('.');
-			if ((dot > -1) && (dot < (filename.length()))) {
-				return filename.substring(0, dot);
+		number += 1;
+		int start, end;
+		if (outputPath.equals("")) {
+			end = filename.indexOf('.');
+			if ((end > -1) && (end < (filename.length()))) {
+				return filename.substring(0, end) + extName;
 			}
-		}
-		return filename;
-	}
-	public static String getName0(String filename) {
-		if ((filename != null) && (filename.length() > 0)) {
-			int start = filename.lastIndexOf('/');
-			int end=filename.indexOf('.');
-			if ((start > -1) && (start < (filename.length()))&&(end> -1)&&(end<(filename.length()))) {
-				return filename.substring(start, end);
+		} else if ((filename != null) && (filename.length() > 0)) {
+			start = filename.lastIndexOf('/');
+			end = filename.indexOf('.');
+			if ((start > -1) && (start < (filename.length())) && (end > -1) && (end < (filename.length()))) {
+				return filename.substring(start, end) + extName;
 			}
 		}
 		return filename;
